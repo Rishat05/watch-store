46fbd60e09038f3318ad5358a96e7baa9dc53e4a
rishat <rishatrifa56@gmail.com>
2022-04-05 04:41:10 +0600

    question part done

--- src/components/CustomerReview/CustomerReview.js
+++ src/components/CustomerReview/CustomerReview.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './CustomerReview.css';
 const CustomerReview = (props) => {
-    const { name, img, comment } = props.review;
+    const { name, img, comment, rating } = props.review;
     return (
         <div className="review-post">
             <img className='review-img' src={img} alt="" />
             <h3>{name}</h3>
+            <div className="ratings">
+                <i class="fas fa-star filled"></i>
+                <i class="fas fa-star filled"></i>
+                <i class="fas fa-star filled"></i>
+                <i class="fas fa-star filled"></i>
+                <i class="fas fa-star empty"></i>
+                <span class="rating-point">{rating}</span>
+            </div>
             <p>{comment}</p>
 
         </div>
